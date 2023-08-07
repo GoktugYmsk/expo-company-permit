733bb8edb13a6add83a9c7a7cef7dfa308f47bff
GoktugYmsk <gogtug033@outlook.com>
2023-08-07 15:45:06 +0300

    components are created

---
+++ components/menu/home/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+function Home() {
+
+    return (
+        <View>
+            <Text>
+                İzinlilerin Listesi
+            </Text>
+            <Text>
+                Ahmet
+            </Text>
+            <Text>
+                Mehmet
+            </Text>
+            <Text>
+                Cemal
+            </Text>
+        </View>
+    )
+}
+
+export default Home
