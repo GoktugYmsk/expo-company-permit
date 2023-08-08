e6a6d087377634f50716c2bdce369fed6df14660
GoktugYmsk <gogtug033@outlook.com>
2023-08-08 08:45:03 +0300

    signnp page edited

--- components/signUp/index.jsx
+++ components/signUp/index.jsx
@@ -1,13 +1,78 @@
-import React from 'react'
-
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 function SignUp() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('')
+
+    const navigation = useNavigation()
+
+    const handleSignUp = () => {
+        navigation.navigate('Login')
+    };
+
     return (
-        <View>
-            <Text>signup ekranı</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Kaydol</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Adınız"
+                value={name}
+                onChangeText={setName}
+                keyboardType="name"
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="E-posta"
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Şifre"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={true}
+            />
+            <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+                <Text style={styles.buttonText}>Kaydol</Text>
+            </TouchableOpacity>
         </View>
-    )
+    );
 }
 
-export default SignUp
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+    },
+    input: {
+        width: '80%',
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+        paddingHorizontal: 10,
+    },
+    button: {
+        backgroundColor: 'blue',
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 10,
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+    },
+});
+
+export default SignUp;
