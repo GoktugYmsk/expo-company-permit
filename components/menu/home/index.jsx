297dda223502ac74004326f00739aa4063a28cd7
berkan-ridvan <berkan5551@gmail.com>
2023-08-09 19:03:30 +0300

    first and second pages

--- components/menu/home/index.jsx
+++ components/menu/home/index.jsx
@@ -57,29 +57,6 @@ const styles = StyleSheet.create({
         fontSize: 24,
         marginBottom: 20,
     },
-    centeredView: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 22,
-    },
-    modalView: {
-        margin: 20,
-        backgroundColor: 'white',
-        borderRadius:20,
-        width: '90%',
-        padding: 15,
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-
-    },
 });
 
 export default Home;
