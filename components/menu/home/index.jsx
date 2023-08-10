47cc042b2b3cc43c0b7771fb470c4b5347dc1bc9
GoktugYmsk <gogtug033@outlook.com>
2023-08-10 19:45:09 +0300

    bugs are fixed

--- components/menu/home/index.jsx
+++ components/menu/home/index.jsx
@@ -1,84 +1,74 @@
-import { useNavigation } from "@react-navigation/native";
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View, Modal } from "react-native";
-import { Calendar, DotMarking } from "react-native-calendars";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Calendar } from "react-native-calendars";
 import { useSelector } from "react-redux";
 import Accordion from "../../UI/Accordion";
 
-const users = [
-  {
-    name: "Ahmet",
-    date: "2023-08-16",
-  },
-];
-
 function Home() {
   const [selectedDate, setSelectedDate] = useState(new Date());
-  const [requests, setRequests] = useState(users);
+  const [permitsOnCalendar, setPermitsOnCalendar] = useState([]);
 
   const workerInfo = useSelector((state) => state.workerInfoTotal.workerInfo);
-  const workerPerReq = useSelector(
-    // --> workerPerReq.filter(workerPerReq.map()) gibi bir mantıkla filtreleme olacak.
-    (state) => state.workerInfoTotal.workerPerReq
-  );
-
   const formattedSelectedDate = selectedDate.toISOString().split("T")[0];
 
-  const filteredUsers = requests.filter(
-    (item) => item.date === formattedSelectedDate
-  );
-    const permitsOnCalender = workerInfo.filter(permit => {
-      const dateFrom = permit.startDay;
-      const dateTo = permit.endDay;
-      const dateCheck = formattedSelectedDate;
-  
-      const d1 = dateFrom.split("-");
-      const d2 = dateTo.split("-");
-      const c = dateCheck.split("-");
-  
-      const from = new Date(d1[2], parseInt(d1[1]) - 1, d1[0]);
-      const to = new Date(d2[2], parseInt(d2[1]) - 1, d2[0]);
-      const check = new Date(c[2], parseInt(c[1]) - 1, c[0]);
-      if (from <= check && check <= to) {
-        return permit;
-      } 
-    })
+  const updatePermitsOnCalendar = (selectedDay) => {
+    const newSelectedDate = new Date(selectedDay.dateString);
 
-    console.log(permitsOnCalender)
+    if (newSelectedDate.getTime() !== selectedDate.getTime()) {
+      setSelectedDate(newSelectedDate);
+
+      if (workerInfo) {
+        const permitsOnSelectedDate = workerInfo.filter((user) => {
+          const startDate = new Date(user.startDay);
+
+          if (user.endDay) {
+            const endDate = new Date(user.endDay);
+            return startDate <= newSelectedDate && endDate >= newSelectedDate;
+          }
+
+          return startDate.getTime() === newSelectedDate.getTime();
+        });
+
+        setPermitsOnCalendar(permitsOnSelectedDate);
+      } else {
+        console.log("Worker Info is empty");
+        setPermitsOnCalendar([]);
+      }
+    }
+  };
+
 
   return (
     <View style={styles.container}>
       <Calendar
         style={styles.calendar}
         current={selectedDate}
-        onDayPress={(day) => setSelectedDate(new Date(day.dateString))}
+        onDayPress={updatePermitsOnCalendar}
         monthFormat={"yyyy MMMM"}
         markingType={"multi-dot"}
         hideExtraDays={true}
       />
-      <Text style={styles.permitTitle}>{formattedSelectedDate} tarihinde izinli olan çalışanlar:</Text>
-      {permitsOnCalender.length > 0 ? (
-        permitsOnCalender.map((user, index) => {
-          return (
-            <Accordion
-              key={index}
-              title={user.name}
-              content={
-                <View>
-                  <Text style={styles.contentText}>İzin Sebebi : {user.reason}</Text>
-                  <Text style={styles.contentText}>Başlangıç Tarihi : {user.startDay}</Text>
-                  <Text style={styles.contentText}>Bitiş Tarihi : {user.endDay}</Text>
-                  <Text style={styles.contentText}>Yönetici : {user.manager}</Text>
-                </View>
-              }
-            >
-            </Accordion>
-          )
-        })
+      <Text style={styles.permitTitle}>
+        {formattedSelectedDate} tarihinde izinli olan çalışanlar:
+      </Text>
+      {permitsOnCalendar.length > 0 ? (
+        permitsOnCalendar.map((user, index) => (
+          <Accordion
+            key={index}
+            title={user.name}
+            content={
+              <View>
+                <Text style={styles.contentText}>İzin Sebebi : {user.reason}</Text>
+                <Text style={styles.contentText}>Başlangıç Tarihi : {user.startDay}</Text>
+                <Text style={styles.contentText}>Bitiş Tarihi : {user.endDay || "Belirtilmemiş"}</Text>
+                <Text style={styles.contentText}>Yönetici : {user.manager}</Text>
+              </View>
+            }
+          />
+        ))
       ) : (
-        <Text>İzinli çalışan bulunamadı.</Text>
+        <Text>Seçili tarihte izinli çalışan bulunamadı.</Text>
       )}
-
     </View>
   );
 }
@@ -94,19 +84,15 @@ const styles = StyleSheet.create({
     width: 410,
     marginTop: 10,
   },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
   permitTitle: {
-    fontSize:18,
-    fontWeight:'bold',
-    marginTop:8 , 
-    marginBottom:4
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 8,
+    marginBottom: 4,
   },
-  contentText : {
-    fontSize:15 ,
-  }
+  contentText: {
+    fontSize: 15,
+  },
 });
 
 export default Home;
