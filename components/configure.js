5d038b9ea4b2fe71804ef237dae487c24ef6cabb
GoktugYmsk <gogtug033@outlook.com>
2023-08-14 18:37:57 +0300

    bugs are fixed

--- components/configure.js
+++ components/configure.js
@@ -1,147 +1,68 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useEffect } from 'react'
-import { useState } from 'react';
+import { createSlice } from "@reduxjs/toolkit";
 
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-import { useSelector } from 'react-redux';
-import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+const initialState = {
+    reason: [],
+    startDay: [],
+    endDay: [],
+    manager: '',
+    manageName: '',
+    worker: [],
+    workerInfo: [],
+    workerPerReq: [],   //--> bütün gönderilen izinler burada gözüküyor.
+    allPermits: [],
+    isWorkerPermit: false,
+    hamburgerActive: false,
+    regUser: [],
+    idControl: null,
 
-
-function Menu() {
-    const navigation = useNavigation();
-
-    const manageName = useSelector((state) => state.management.manageName);
-    const manager = useSelector((state) => state.management.manager)
-    const worker = useSelector((state) => state.workerInfoTotal.worker);
-
-    const handleMainClick = () => {
-        if (manager) {
-            navigation.navigate('Home')
-        }
-        else {
-            alert('Profile sayfasından yönetici Seçimi yapınız')
-            // setTimeout(() => {
-            //     navigation.navigate('Profile')
-            // }, 3000);
-        }
-    }
-
-    const handleRequestClick = () => {
-        if (manager) {
-            navigation.navigate('PerRequest')
-        }
-        else {
-            alert('Profile sayfasından yönetici Seçimi yapınız')
-            // setTimeout(() => {
-            //     navigation.navigate('Profile')
-            // }, 3000);
-        }
-    }
-
-    const handleApprovalClick = () => {
-        if (manageName && worker) {
-            navigation.navigate('Approval')
-        }
-        else if (!manageName && worker && manager) {
-            navigation.navigate('MyRequest')
-        }
-        else if (!manageName && worker && !manager) {
-            alert('Profile sayfasından yönetici Seçimi yapınız')
-            // setTimeout(() => {
-            //     navigation.navigate('Profile')
-            // }, 3000);
-        }
-    }
-
-    const isAdmin = manageName !== '';
-
-    return (
-        <View style={styles.container}  >
-            <View style={styles.header}>
-                <Text style={styles.headerText}>Pinsoft İzinlerim</Text>
-            </View>
-
-            <View style={styles.menu}>
-                <TouchableOpacity style={styles.button} onPress={handleMainClick} >
-                    <Icon name="home" size={30} color="#8754ce" />
-                    <Text style={styles.buttonText}>Anasayfa</Text>
-                    <Icon name="arrow-right" size={23} color="#6d6e70" />
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={handleProfileClick} >
-                    <Icon name="account" size={30} color="#8754ce" />
-                    <Text style={styles.buttonText}>Profil</Text>
-                    <Icon name="arrow-right" size={23} color="#6d6e70" />
-                </TouchableOpacity>
-
-                {!manageName &&
-                    <TouchableOpacity style={styles.button} onPress={handleRequestClick} >
-                        <Icon name="thumb-up" size={30} color="#8754ce" />
-                        <Text style={styles.buttonText}>İzin talebi</Text>
-                        <Icon name="arrow-right" size={23} color="#6d6e70" />
-                    </TouchableOpacity>
-                }
-
-                {
-                    isAdmin &&
-                    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('OffDuty')}>
-                        <Icon name="sunglasses" size={30} color="#8754ce" />
-                        <Text style={styles.buttonText}>İzinlilerim</Text>
-                        <Icon name="arrow-right" size={23} color="#6d6e70" />
-                    </TouchableOpacity>
-                }
-                <TouchableOpacity style={styles.button} onPress={handleApprovalClick} >
-                    <Icon name="progress-clock" size={30} color="#8754ce" />
-                    <Text style={styles.buttonText}>Onay Bekleyen İşlemler</Text>
-                    <Icon name="arrow-right" size={23} color="#6d6e70" />
-                </TouchableOpacity>
-            </View>
-        </View >
-    );
 }
-const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        height: '100%',
-        padding: 10,
-    },
-    menu: {
-        width: '100%',
-        padding: 20,
-        paddingRight: 10,
-        paddingLeft: 10,
-    },
-    header: {
-        backgroundColor: '#8754ce',
-        width: '100%',
-        padding: 10,
-        borderRadius: 4,
-    },
-    headerText: {
-        fontSize: 20,
-        width: '100%',
-        textAlign: "center",
-        fontWeight: "bold",
-        color: 'white',
-    },
-    button: {
-        flexDirection: 'row',
-        backgroundColor: '#f8f8f8',
-        padding: 10,
-        borderRadius: 5,
-        marginTop: 10,
-        alignItems: "center",
-        paddingLeft: 10,
-        borderWidth: 1,
-        borderColor: '#c4c4c4',
-    },
-    buttonText: {
-        width: '80%',
-        paddingLeft: 10,
-    },
-});
 
+export const configure = createSlice({
+    name: 'control',
+    initialState,
+    reducers: {
+        setReason: (state, action) => {
+            state.reason = action.payload;
+        },
+        setStartDay: (state, action) => {
+            state.startDay = action.payload;
+        },
+        setEndDay: (state, action) => {
+            state.endDay = action.payload;
+        },
+        setManager: (state, action) => {
+            state.manager = action.payload;
+        },
+        setManageName: (state, action) => {
+            state.manageName = action.payload;
+        },
+        setWorker: (state, action) => {
+            state.worker = action.payload;
+        },
+        setWorkerInfo: (state, action) => {
+            state.workerInfo = action.payload;
+        },
+        setWorkerPerReq: (state, action) => {
+            state.workerPerReq = action.payload;
+        },
+        setAllPermits: (state, action) => {
+            state.allPermits = action.payload
+        },
+        setIsWorkerPermit: (state, action) => {
+            state.isWorkerPermit = action.payload
+        },
+        setHamburgerActive: (state, action) => {
+            state.hamburgerActive = action.payload
+        },
+        setRegUser: (state, action) => {
+            state.regUser = action.payload
+        },
+        setIdControl: (state, action) => {
+            state.idControl = action.payload
+        },
+    }
+})
 
-export default Menu
+export const { setPassengerAmount, setReason, setStartDay, setEndDay, setManager, setManageName, setWorker, setWorkerInfo, setWorkerPerReq, setAllPermits, setIsWorkerPermit, setHamburgerActive, setRegUser, setIdControl } = configure.actions
+
+export default configure.reducer;
