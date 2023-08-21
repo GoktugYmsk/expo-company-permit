01803ec7220e2ae09083ad008bdcca512cb94575
GoktugYmsk <gogtug033@outlook.com>
2023-08-21 12:46:21 +0300

    bugs are fixed

--- intercepter.js
+++ intercepter.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: 'http://localhost:8080/', // API kök URL'nizi buraya ekleyin
+    baseURL: 'http://localhost:8080/',
 });
 
 api.interceptors.request.use(
     async (config) => {
         const tokenLocale = localStorage.getItem('userToken')
-        const token = { tokenLocale }; // Kullanıcı oturum açma token'ınız
+        const token = { tokenLocale };
         if (token) {
             config.headers['Authorization'] = `Bearer ${tokenLocale}`;
         }
